96469d4039d3366fee701a27d8e69cf3ae2705b4
mamiaomiao <850178072@qq.com>
2018-04-09 08:59:26 +0800

    Add files via upload

---
+++ lesson15.js
@@ -0,0 +1,54 @@
+//iterator   for...of
+
+{
+	let arr =['hello','world'];
+	let map = arr[Symbol.iterator]();
+	console.log(map.next());
+	console.log(map.next());
+	console.log(map.next());
+}
+{
+	//自定义iterator函数
+	let obj = {
+		start:[1,3,2],
+		end:[4,6,8],
+		[Symbol.iterator](){
+			let self = this;
+			let index = 0;
+			let arr = self.start.concat(self.end);
+			let len = arr.length;
+			return {
+				next(){
+					if(index<len){
+						return {
+							value:arr[index++],
+							done:false
+						}
+					}else{
+						return {
+							value:arr[index++],
+							done:true
+						}
+					}
+				}
+			}
+		}
+	}
+	for(let key of obj){
+		console.log(key);
+	}
+
+}
+{
+	let arr =['hello','world'];
+	for(let value of arr){
+		console.log(value);
+	}
+}
+
+
+
+
+
+
+
